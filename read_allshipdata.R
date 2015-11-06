527e1d5a0161649cf4874edc6f4e52dc627533ec
codykoebnick <codykoebnick@Codys-MacBook-Air.local>
2015-11-06 13:01:20 +0100

    Set Cody's WD and import stuff

--- read_allshipdata.R
+++ read_allshipdata.R
@@ -1,12 +1,16 @@
 # Import the data about piracy attacks into 
 
 # set working directories 
-try(setwd("/Users/codykoebnick/Downloads/MaritimePiracy"))
-try(setwd("E:/bjoer/Documents/Google Drive/Universität/Hertie/03_Fall 2015/05_Master Thesis/00_Piracy_2015-16/03_Data/Traxler"))
+try(setwd("/Users/codykoebnick/Downloads/Data Set"))
+try(setwd("E:/bjoer/Documents/Google Drive/Universit?t/Hertie/03_Fall 2015/05_Master Thesis/00_Piracy_2015-16/03_Data/Traxler"))
+getwd()
 
 #import data with the rio package the swiss army knife
 library(rio)
 shipping <- import('all_shipdata_update.dta')
+
+######## Cody's import craziness
+####install.packages("haven")
 
 #estimate model
 logit1 <- glm(notyemen ~ as.factor(shiptype) + as.factor(shipcategory), data = shipping, familiy = "binominal")
