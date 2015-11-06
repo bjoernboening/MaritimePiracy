859c21900f4cf158b3814767106e431143a09214
bjoernboening <bjoboe@t-online.de>
2015-11-06 12:54:28 +0100

    clean working directories
    #6

--- read_allshipdata.R
+++ read_allshipdata.R
@@ -2,11 +2,10 @@
 
 # set working directories 
 try(setwd("/Users/codykoebnick/Downloads/MaritimePiracy"))
-try(setwd("bjorns"))
+try(setwd("E:/bjoer/Documents/Google Drive/Universität/Hertie/03_Fall 2015/05_Master Thesis/00_Piracy_2015-16/03_Data/Traxler"))
 
 #import data with the rio package the swiss army knife
 library(rio)
-setwd("E:/bjoer/Documents/Google Drive/Universit?t/Hertie/03_Fall 2015/05_Master Thesis/00_Piracy_2015-16/03_Data/Traxler")
 shipping <- import('all_shipdata_update.dta')
 
 #estimate model
