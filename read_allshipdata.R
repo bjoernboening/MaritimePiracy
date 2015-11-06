faa4c37cb36db0df548ff49a93c9b7efd76ac4c9
codykoebnick <codykoebnick@Codys-MacBook-Air.local>
2015-11-06 12:44:35 +0100

    set working directories
    #3

--- read_allshipdata.R
+++ read_allshipdata.R
@@ -1,7 +1,9 @@
 # Import the data about piracy attacks into 
 
+# set working directories 
+
 library(rio)
-setwd("E:/bjoer/Documents/Google Drive/Universität/Hertie/03_Fall 2015/05_Master Thesis/00_Piracy_2015-16/03_Data/Traxler")
+setwd("E:/bjoer/Documents/Google Drive/Universit?t/Hertie/03_Fall 2015/05_Master Thesis/00_Piracy_2015-16/03_Data/Traxler")
 shipping <- import('all_shipdata_update.dta')
 
 #estimate model
