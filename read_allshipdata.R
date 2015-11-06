bb2443e325cd43dc2e0b8ef045de51b60c7cad58
codykoebnick <codykoebnick@Codys-MacBook-Air.local>
2015-11-06 12:49:56 +0100

    4

--- read_allshipdata.R
+++ read_allshipdata.R
@@ -1,7 +1,10 @@
 # Import the data about piracy attacks into 
 
 # set working directories 
+try(setwd("cody"))
+try(setwd("bjorns"))
 
+#import data with the rio package the swiss army knife
 library(rio)
 setwd("E:/bjoer/Documents/Google Drive/Universit?t/Hertie/03_Fall 2015/05_Master Thesis/00_Piracy_2015-16/03_Data/Traxler")
 shipping <- import('all_shipdata_update.dta')
