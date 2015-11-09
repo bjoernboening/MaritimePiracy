89dc699c208c5c08f5bb930b864090f15896095f
codykoebnick <codykoebnick@Codys-MacBook-Air.local>
2015-11-09 15:37:46 +0100

    Added a pro bit regression. Can someone verify it's validity?

--- read_allshipdata.R
+++ read_allshipdata.R
@@ -1,23 +1,31 @@
 # Import the data about piracy attacks into 
   # Libraries we need for the project, make sure you have them installed
 library(rio)
-library(haven)
 library(dplyr)
+
 
 # set working directories 
 try(setwd("/Users/codykoebnick/Downloads/Data Set"))
-try(setwd("E:/bjoer/Documents/Google Drive/Universität/Hertie/03_Fall 2015/05_Master Thesis/00_Piracy_2015-16/03_Data/Tennessee"))
+try(setwd("E:/bjoer/Documents/Google Drive/Universit?t/Hertie/03_Fall 2015/05_Master Thesis/00_Piracy_2015-16/03_Data/Tennessee"))
 getwd()
 
 #import data with the rio package the swiss army knife
 shipping <- read.csv("MaritimePiracyTennessee.csv", header = TRUE, sep = ";")
-  # Cody's import craziness
-  # install.packages("haven")
+
 # try to remove missing values from incident_type, so far no success
 attacks <- shipping %>% filter(!is.na(incident_type) %>% droplevels()
 str(attacks)
 
-# Basic descriptive statistics 
+#attempting a probit regression 
+shipping$timeofdayrecode <- factor(shipping$timeofdayrecode)
+shipping$vessel_type <- factor(shipping$vessel_type)
+shipping$vessel_status <- factor(shipping$vessel_status)
+shipping$Incident_type_recode <- factor(shipping$Incident_type_recode)
+
+xtabs(~ timeofdayrecode + Incident_type_recode, data=shipping)
+
+myprobit <- glm(Incident_type_recode ~ timeofdayrecode + vessel_type + vessel_status, family=binomial(link="probit"), data=shipping)
+summary (myprobit)
 
 
 #estimate model
