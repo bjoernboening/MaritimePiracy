403c44d467ba170791e6ab734bdb256f65d65d67
bjoernboening <bjoboe@t-online.de>
2015-11-11 21:12:49 +0100

    Code cleaning: Added parts for descriptive stats / deactivated attach() command
    #14 Added header, section for descriptive stats, deactivated attach()
    command to avoid confusion.

--- read_allshipdata.R
+++ read_allshipdata.R
@@ -1,24 +1,36 @@
-# Import the data about piracy attacks into 
-  # Libraries we need for the project, make sure you have them installed
+#########################################################
+#### Maritime Piracy Data Analysis ######################
+#########################################################
+#### by Laurence Hendry, Cody Koebnick, and Björn Boening
+
+# Import the dataset about piracy attacks into your wd 
+  # Call libraries we need for the project, make sure you have them installed
 library(rio)
 library(dplyr)
 library(tidyr)
 library(ggplot2)
-attach(shipping)
+#attach(shipping)
 
 # set working directories 
 try(setwd("/Users/codykoebnick/Downloads/Data Set"))
 try(setwd("E:/bjoer/Documents/Google Drive/Universität/Hertie/03_Fall 2015/05_Master Thesis/00_Piracy_2015-16/03_Data/Tennessee"))
 getwd()
 
-#import data with the rio package the swiss army knife
+#import data
   # empty cells are now coded with NA and can manually be excluded from any function with na.omit command
 shipping <- read.csv("MaritimePiracyTennessee.csv", header = TRUE, sep = ";", stringsAsFactors = TRUE, na.strings = c("", "NA"))
   # have a look at how the variables are created
 str(shipping)
+  # creae unique identifier country year
+  # GET external source data
 
 # combine date into one column
 unite(shipping, "date", c("year", "month", "day"), sep = "-")
+
+
+#######################################
+## Descriptive Statistics
+#######################################
 
 
 #attempting a probit regression 
@@ -37,4 +49,6 @@ summary (myprobit)
 logit1 <- glm(notyemen ~ as.factor(shiptype) + as.factor(shipcategory), data = shipping, familiy = "binominal")
 lm(logit1)
 
-## End of code
+source()
+###############################
+### End of script
