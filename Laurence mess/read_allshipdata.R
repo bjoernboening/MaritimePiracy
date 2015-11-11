fd235ffb840a441d81991fda788b62b6c36480e4
laurencehendry <lmghendry@gmail.com>
2015-11-11 15:44:00 +0100

    Laurence web scrape attempt

---
+++ Laurence mess/read_allshipdata.R
@@ -0,0 +1,56 @@
+# Import the data about piracy attacks into 
+  # Libraries we need for the project, make sure you have them installed
+library(rio)
+library(dplyr)
+library(XML)
+library(rvest)
+library(stringr)
+
+# set working directories 
+try(setwd("/Users/codykoebnick/Downloads/Data Set"))
+try(setwd("E:/bjoer/Documents/Google Drive/Universit?t/Hertie/03_Fall 2015/05_Master Thesis/00_Piracy_2015-16/03_Data/Tennessee"))
+try(setwd("//Users/laurencehendry/GoogleDrive/Master Thesis - Shared/MPP-E1180 - Introduction to Collaborative Social Science Data Analysis")) 
+getwd()
+
+#import data with the rio package the swiss army knife
+shipping <- read.csv("MaritimePiracyTennessee.csv", header = TRUE, sep = ";")
+
+# try to remove missing values from incident_type, so far no success
+attacks <- shipping %>% filter(!is.na(incident_type) %>% droplevels()
+str(attacks)
+
+#attempting a probit regression 
+shipping$timeofdayrecode <- factor(shipping$timeofdayrecode)
+shipping$vessel_type <- factor(shipping$vessel_type)
+shipping$vessel_status <- factor(shipping$vessel_status)
+shipping$Incident_type_recode <- factor(shipping$Incident_type_recode)
+
+xtabs(~ timeofdayrecode + Incident_type_recode, data=shipping)
+
+myprobit <- glm(Incident_type_recode ~ timeofdayrecode + vessel_type + vessel_status, family=binomial(link="probit"), data=shipping)
+summary (myprobit)
+
+
+#estimate model
+logit1 <- glm(notyemen ~ as.factor(shiptype) + as.factor(shipcategory), data = shipping, familiy = "binominal")
+lm(logit1)
+
+#scrapes the WB for data on country GDP
+URL <- ("http://data.worldbank.org/indicator/ny.gdp.pcap.cd/countries")
+#Gather content and parse all tables
+tables <- URL %>% GET() %>%
+content(as = "parsed") %>%
+readHTMLTable()
+
+
+
+
+
+
+
+
+
+
+
+
+## End of code
