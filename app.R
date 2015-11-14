2b7ee80765a47fdf047ec72f67a1465a6ec5448e
bjoernboening <bjoboe@t-online.de>
2015-11-14 01:35:37 +0100

    dataset is stored locally, added sorry

--- app.R
+++ app.R
@@ -13,6 +13,7 @@ library(gsubfn)
 
 ### Prepare Coordinates
 Attacken <- read.csv("E:/bjoer/Documents/Google Drive/Universität/Hertie/03_Fall 2015/05_Master Thesis/00_Piracy_2015-16/03_Data/Tennessee/MaritimePiracyTennessee.csv", header = TRUE, sep = ";", stringsAsFactors = FALSE)
+# sorry! ask for the dataset if you are curios.
 Attacken$longitude <- gsub(",", ".", Attacken$longitude)
 Attacken$latitude <- gsub(",", ".", Attacken$latitude)
 Attacken$longitude <- as.numeric(Attacken$longitude)
