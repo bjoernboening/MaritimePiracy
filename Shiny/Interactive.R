3853b6f6ed7d597c17fff881d7fd5ff72c2c85bc
bjoernboening <bjoboe@t-online.de>
2015-11-29 22:07:18 +0100

    New shiny attempts - no success

--- Shiny/Interactive.R
+++ Shiny/Interactive.R
@@ -16,10 +16,36 @@ Attacken$latitude <- gsub(",", ".", Attacken$latitude)
 Attacken$longitude <- as.numeric(Attacken$longitude)
 Attacken$latitude <- as.numeric(Attacken$latitude)
 
-Coordinates <- select(Attacken, 9 10) 
-Coordinates <- data.frame(matrix(unlist(Coordinates), nrow=646, byrow=T)) # Convert to dataframe
+Coordinates <- select(Attacken, 9, 10) 
+Coordinates <- data.frame(matrix(unlist(Coordinates), nrow=6434, byrow=T)) # Convert to dataframe
 Coordinates <- select(Coordinates, c(Longitude=X1, Latitude=X2, Height=X3)) # Rename
 
+###Interactive Test
+outline <- Attacken[Attacken(Attacken$longitude, Attacken$latitude),]
+
+data(Coordinates)
+
+# Show first 20 rows from the `quakes` dataset
+leaflet(data = Coordinates[1:20,]) %>% addTiles() %>%
+  addMarkers(~long, ~lat, popup = ~as.character(mag))
+
+map <- leaflet(quakes) %>%
+  # Base groups
+  addTiles(group = "OSM (default)") %>%
+  addProviderTiles("Stamen.Toner", group = "Toner") %>%
+  addProviderTiles("Stamen.TonerLite", group = "Toner Lite") %>%
+  # Overlay groups
+  addCircles(~long, ~lat, ~10^mag/5, stroke = F, group = "Quakes") %>%
+  addPolygons(data = outline, lng = ~long, lat = ~lat,
+              fill = F, weight = 2, color = "#FFFFCC", group = "Outline") %>%
+  # Layers control
+  addLayersControl(
+    baseGroups = c("OSM (default)", "Toner", "Toner Lite"),
+    overlayGroups = c("Quakes", "Outline"),
+    options = layersControlOptions(collapsed = FALSE)
+  )
+map
+###End of Interactive Test
 ### Set color (for dots?)
 r_colors <- rgb(t(col2rgb(colors()) / 255))
 names(r_colors) <- colors()
@@ -27,7 +53,7 @@ names(r_colors) <- colors()
 ui <- fluidPage(
   leafletOutput("mymap"),
   p()
-  )
+)
 
 server <- function(input, output, session) {
   # for background calculation
